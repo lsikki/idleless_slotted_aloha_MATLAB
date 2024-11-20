e2235007016c89a6ebc31302aa20b00b2d384571
lsikki <108148866+lsikki@users.noreply.github.com>
2024-11-20 10:20:53 -0500

    Added a throughput vs access probability simulation, made some changes to overall structure

--- IdlelessSlottedALOHA.m
+++ IdlelessSlottedALOHA.m
@@ -1,59 +1,46 @@
 
-% Here we are initializing all parameters
-drones = [Drone(1), Drone(2), Drone(3), Drone(4), Drone(5)];
-access_probabilities = 0.3 * rand() * ones(1, length(drones)); % all have the same transmission probability
-gcs = GCStation();
-slots_per_frame = 5;
-total_frames = 1;
-successful_drones = [];
-total_successful_transmissions = 0;
-
-% Now we will initiate a random access session 
-% Part of the beacon is the access probability for each drone (the same for all)
-drones = gcs.send_beacon(drones, access_probabilities);
-
-for f = 1:total_frames
-    disp(['-----------FRAME: ', num2str(f), '-----------']);
-    for t = 1:slots_per_frame
-        disp(['---SLOT: ', num2str(t), '---']);
-        transmitting_drones = [];
+function [throughput, access_probability] = IdlelessSlottedALOHA(gcs, drones, slots_per_frame, total_frames)
+    access_probabilities = 0.3 * rand() * ones(1, length(drones)); % all have the same transmission probability    
+    successful_drones = [];
+    total_successful_transmissions = 0;
     
-        for i = 1:length(drones)
-           drones(i) = drones(i).decide_to_transmit(); 
-           if drones(i).state == 1
-            transmitting_drones = [transmitting_drones, drones(i)]; 
-           end
+    % Now we will initiate a random access session 
+    % Part of the beacon is the access probability for each drone (the same for all)
+    drones = gcs.send_beacon(drones, access_probabilities);
+    
+    for f = 1:total_frames
+        disp(['-----------FRAME: ', num2str(f), '-----------']);
+        for t = 1:slots_per_frame
+            disp(['---SLOT: ', num2str(t), '---']);
+            transmitting_drones = [];
+        
+            for i = 1:length(drones)
+               drones(i) = drones(i).decide_to_transmit(); 
+               if drones(i).state == 1
+                transmitting_drones = [transmitting_drones, drones(i)]; 
+               end
+            end
+    
+            disp('Drones attempting to transmit: ')
+            for d=transmitting_drones
+               disp(d.ID)
+            end
+    
+            % keeping track of successful cases to compute throughput later
+            if(length(transmitting_drones)==1)
+                total_successful_transmissions = total_successful_transmissions + 1;
+            end
+    
+            gcs = gcs.receive_incoming_data(transmitting_drones);
         end
-
-        disp('Drones attempting to transmit: ')
-        for d=transmitting_drones
-           disp(d.ID)
-        end
-
-        % keeping track of successful cases to compute throughput later
-        if(length(transmitting_drones)==1)
-            total_successful_transmissions = total_successful_transmissions + 1;
-        end
-
-        gcs = gcs.receive_incoming_data(transmitting_drones);
+        
+        successful_drones = gcs.send_acks();
     end
     
-    successful_drones = gcs.send_acks();
+    total_slots = total_frames * slots_per_frame;
+    throughput = total_successful_transmissions / total_slots;
+    access_probability = access_probabilities(1); % all drones have the same ap for now
+
 end
 
-total_slots = total_frames * slots_per_frame;
-throughput = total_successful_transmissions / total_slots;
-disp('----------------------------------');
-disp('--------Session throughput--------');
-disp(num2str(throughput));
-
-%% TO DO:
-%% 1- add max number of attempts + backoff strategy
-%% 2- in idle slots, the ground station should send ACKS 
-%% 3- compute key statistics e.g., throughput and compare with regular slotted ALOHA
-%% 4- visualize simulation results
-%% 5- drones should wait for a certain period, if no ACK then state=0 and then retry
-
-%% DONE:
-%% Beacon to initiate random access session
 
---
+++ main.m
@@ -0,0 +1,38 @@
+% Here we are initializing all parameters
+drones = [Drone(1), Drone(2), Drone(3), Drone(4), Drone(5)];
+gcs = GCStation();
+slots_per_frame = 5;
+total_frames = 3;
+number_sessions = 10;
+
+throughputs= zeros(number_sessions);
+probabilities = zeros(number_sessions);
+for i = 1:number_sessions
+    [throughput, probability] = IdlelessSlottedALOHA(gcs, drones, slots_per_frame, total_frames);
+    throughputs(i) = throughput;
+    probabilities(i) = probability;
+end
+
+
+figure;  
+scatter(probabilities, throughputs, 100, 'filled', 'MarkerEdgeColor', 'k'); 
+xlabel('Access Probability');
+ylabel('Throughput');
+title('Throughput vs Access Probability for Slotted ALOHA');
+grid on;  
+
+
+
+
+
+
+
+%% TO DO:
+%% 1- add max number of attempts + backoff strategy
+%% 2- in idle slots, the ground station should send ACKS 
+%% 3- compute key statistics e.g., throughput and compare with regular slotted ALOHA
+%% 4- visualize simulation results
+%% 5- drones should wait for a certain period, if no ACK then state=0 and then retry
+
+%% DONE:
+%% Beacon to initiate random access session
