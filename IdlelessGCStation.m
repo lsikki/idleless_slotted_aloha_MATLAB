9b59745abcabd401c297e1b64209ce35601edd17
Kinjal-Aneesh <kgheraw1@lakeheadu.ca>
2024-11-24 22:49:16 -0500

    Update IdlelessGCStation.m

--- IdlelessGCStation.m
+++ IdlelessGCStation.m
@@ -1,15 +1,15 @@
 classdef IdlelessGCStation
     properties
-        ACK_delay = 1;  % Time delay for sending ACKs
+        ACK_delay = 1;  
     end
     
     methods
         function obj = IdlelessGCStation()
-            % Constructor for IdlelessGCStation class
+            
         end
         
         function obj = receive_incoming_data(obj, transmitting_drones)
-            % Process the data received from the transmitting drones
+            
             disp('Receiving data from drones...');
             for i = 1:length(transmitting_drones)
                 drone = transmitting_drones(i);
@@ -18,7 +18,7 @@ classdef IdlelessGCStation
         end
         
         function obj = send_ACKs(obj)
-            % Simulate ACKs being sent by the ground control station in idle slots
+           
             disp('Idle slot detected. Sending ACKs.');
         end
     end
