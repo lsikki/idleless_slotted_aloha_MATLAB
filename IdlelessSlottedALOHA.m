d017c3b72cb8f871e62f30c4e870a3bdd46e2818
Kinjal-Aneesh <kgheraw1@lakeheadu.ca>
2024-11-24 22:48:51 -0500

    Update IdlelessSlottedALOHA.m

--- IdlelessSlottedALOHA.m
+++ IdlelessSlottedALOHA.m
@@ -3,17 +3,17 @@ function [throughput, access_probability] = IdlelessSlottedALOHA(gcs, drones, sl
     total_transmissions = 0;
     total_collisions = 0;
     
-    % Simulating over multiple frames
+    
     for frame = 1:total_frames
         disp(['-----------FRAME: ', num2str(frame), '-----------']);
         successful_transmissions = 0;
         transmitting_drones = [];
 
-        % Simulating each slot within the frame
+        
         for slot = 1:slots_per_frame
             disp(['---SLOT: ', num2str(slot), '---']);
             
-            drones_in_slot = []; % Keep track of which drones are attempting to transmit
+            drones_in_slot = []; 
             for i = 1:length(drones)
                 drone = drones(i).decide_to_transmit();
                 if drone.state == 1
@@ -22,14 +22,14 @@ function [throughput, access_probability] = IdlelessSlottedALOHA(gcs, drones, sl
                 end
             end
             
-            % If no drones are attempting to transmit, ground station sends ACK
+           
             if isempty(drones_in_slot)
                 gcs = gcs.send_ACKs();
             else
                 disp('Drones attempting to transmit:');
                 disp(drones_in_slot);
                 
-                % Handling transmission and collisions
+                
                 if length(drones_in_slot) == 1
                     successful_transmissions = successful_transmissions + 1;
                 else
@@ -38,7 +38,7 @@ function [throughput, access_probability] = IdlelessSlottedALOHA(gcs, drones, sl
             end
         end
         
-        % Update throughput for this frame
+       
         total_transmissions = total_transmissions + successful_transmissions;
     end
     
